9178c8a07dfdb732a31cae8a744fe64007037120
Viktor Nilsson <viktor.w.nilsson@gmail.com>
2017-07-10 21:34:42 +0200

    Pretty shitty validators

--- app/forms.py
+++ app/forms.py
@@ -5,11 +5,14 @@ from wtforms.validators import DataRequired
 
 class RequestForm(FlaskForm):
     stringurl = StringField('stringurl',
-                            [validators.InputRequired()],
+                            [validators.InputRequired(),
+                             # This just checks that it starts correctly
+                             validators.Regexp("^https?:\/\/dayviews.com")],
                             render_kw={"placeholder": 
                             "http://dayviews.com/user/id_of_first_image/"})
     email = StringField('email',
-                        [validators.Email("Please enter your email address.")])
+                        [validators.Email("Please enter your email address."),
+                         validators.InputRequired()])
     username = StringField('username')
     password = PasswordField('password')
     save_text = BooleanField('save_text', default=True)
