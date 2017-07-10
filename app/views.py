7b306e8f69c943fa985227cc193f28e808b562e5
Viktor Nilsson <viktor.w.nilsson@gmail.com>
2017-07-10 22:35:30 +0200

    Pretty shitty and probably not secure but "works"

--- app/views.py
+++ app/views.py
@@ -2,12 +2,8 @@ from flask import flash, render_template, redirect
 from app import app
 from .forms import RequestForm
 
-import bdb_scraper
-
-#@app.route('/')
-#@app.route('/derp')
-#def derp():
-#    return "Hello world"
+import uuid
+from bdb_scraper import bdb_scraper
 
 @app.route('/', methods=['GET', 'POST'])
 def request():
@@ -20,16 +16,25 @@ def request():
 
 def rec(form):
     url = str(form['stringurl'].data)
-    #email = str(form['email'].data)
+    email = str(form['email'].data)
     username = str(form['username'].data)
     password = str(form['password'].data)
     save_text = form['save_text'].data
-    bdb_scraper.scrape(url)
-    return ("received data:"
-            + "<br>url: " + url
-            + "<br>username:" + username
-            + "<br>password" + password
-            + "<br>save_text: " + str(save_text))
-    # render_template('received.html',
-    #                  title="Request received")
+
+    name = bdb_scraper.zip_name_from_url(url) + '-' + uuid.uuid4().hex
+    zip_path = "static/zips/"
+    zip_name = name
+
+    bdb_scraper.scrape(url,
+                       username=username, password=password,
+                       save_text=save_text,
+                       create_zip=True,
+                       zip_name='app/'+zip_path+zip_name,
+                       zip_base='files/',
+                       dest="files/"+name)
+
+    return render_template('done.html',
+                           title='BDB-Scraper',
+                           path=zip_path,
+                           zipname=zip_name+'.zip')
 
