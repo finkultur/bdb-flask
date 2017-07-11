752e450e742eff402b23bbd285230eee7e9c25ec
Viktor Nilsson <viktor.w.nilsson@gmail.com>
2017-07-11 13:25:03 +0200

    threaded=True

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 from app import app
 context = ('cert.pem', 'privkey.pem')
-app.run(debug=True, host="0.0.0.0", ssl_context=context)
+app.run(debug=True, host="0.0.0.0", threaded=True, ssl_context=context)
